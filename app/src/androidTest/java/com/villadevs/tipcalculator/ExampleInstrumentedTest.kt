9ba56833e0656027be22de2044b2dcd489ef8c51
Francisco Villanueva <pacovillano@hotmail.com>
2023-04-21 17:09:27 +0200

    Test Ok

--- app/src/androidTest/java/com/villadevs/tipcalculator/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/villadevs/tipcalculator/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package com.villadevs.tipcalculator
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.hamcrest.CoreMatchers.containsString
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +20,19 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class CalculatorTests {
+
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.villadevs.tipcalculator", appContext.packageName)
+    fun calculate_20_percent_tip() {
+        onView(withId(R.id.etCostOfService))
+            .perform(typeText("50.00"))
+
+        onView(withId(R.id.btCalculate)).perform(click())
+
+        onView(withId(R.id.tvTipResult))
+            .check(matches(withText(containsString("$10.00"))))
     }
 }
--- app/src/main/java/com/villadevs/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/villadevs/tipcalculator/MainActivity.kt
@@ -1,11 +1,68 @@
 package com.villadevs.tipcalculator
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.villadevs.tipcalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btCalculate.setOnClickListener {
+            calculateTip()
+            // Hide the keyboard cuando le des a botón calcular.
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(binding.root.windowToken, 0)
+        }
+
+       //binding.etCostOfService.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
     }
+
+    private fun calculateTip() {
+        val stringTextField = binding.etCostOfService.text.toString()
+        val cost = stringTextField.toDoubleOrNull()
+
+        // If the cost is null or 0, then display 0 tip and exit this function early.
+        if (cost == null || cost == 0.0) {
+            displayTip(0.0)
+            return
+        }
+
+        val tipPerecentage = when (binding.rgTipOptions.checkedRadioButtonId) {
+            R.id.rbOptionTwentyPercent -> 0.20
+            R.id.rbOptionEighteenPercent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = cost.times(tipPerecentage)
+        if (binding.swRoundUp.isChecked) { tip = kotlin.math.ceil(tip) }
+        displayTip(tip)
+    }
+
+    private fun displayTip(tip : Double) {
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tvTipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+
+
 }
